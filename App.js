a5bb10a40dff4d541e0da6cbe5e3541f03ece03c
Nandha <nandha2447@gmail.com>
2018-05-28 20:14:24 -0700

    Adding Entry

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Ionicons} from '@expo/vector-icons'
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <Ionicons name='ios-pizza' color='red' size='100'/>
+        <AddEntry />      
       </View>
     );
   }
